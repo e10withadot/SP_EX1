af69982456311723a5ea13302fd953d0e1729909
e10withadot <e10withadot@gmail.com>
2025-04-02 11:10:48 +0300

    changed vertex to accept array

--- graph.cpp
+++ graph.cpp
@@ -2,8 +2,9 @@ namespace graph {
 	class vertex {
 		public:
 			int edges[]; 
-			vertex(int* edges, int size) {
-				for (int i=0; i < size; i++) {
+			vertex(int edges[]) {
+				int length = sizeof(edges)/sizeof(int);
+				for (int i=0; i < length; i++) {
 					this->edges[i] = edges[i];
 				}
 			}
