fbce223e31fb64b7f1d685e9824c32db1286ccd6
e10withadot <e10withadot@gmail.com>
2025-04-08 17:12:44 +0300

    tests target works

--- tests.cpp
+++ tests.cpp
@@ -1,9 +1,8 @@
 // ey.gellis@gmail.com
-#define DOCTEST_CONFIG
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "algorithms.hpp"
 using namespace graph;
-#include "data_strct.hpp"
 
 TEST_CASE("Testing BFS algorithm") {
     Graph g(6);
@@ -17,7 +16,7 @@ TEST_CASE("Testing BFS algorithm") {
     list<vertex> vertices = g.getVertices();
     list<int> bfs_result = algorithms::bfs(vertices);
 
-    CHECK(bfs_result.size == 6);
+    CHECK(bfs_result.getSize() == 6);
     CHECK(bfs_result.get(0) == 0);
 }
 
@@ -33,7 +32,7 @@ TEST_CASE("Testing DFS algorithm") {
     list<vertex> vertices = g.getVertices();
     list<int> dfs_result = algorithms::dfs(vertices);
 
-    CHECK(dfs_result.size == 6);
+    CHECK(dfs_result.getSize() == 6);
     CHECK(dfs_result.get(0) == 0);
 }
 
@@ -61,9 +60,9 @@ TEST_CASE("Testing Prim's MST algorithm") {
     g.addEdge(2, 3, 5);
 
     list<edge> mst = algorithms::prim(g);
-    CHECK(mst.size == 3);
+    CHECK(mst.getSize() == 3);
     int total_weight = 0;
-    for(int i = 0; i < mst.size; i++) {
+    for(int i = 0; i < mst.getSize(); i++) {
         total_weight += mst.get(i).weight;
     }
     CHECK(total_weight == 6);
@@ -78,9 +77,9 @@ TEST_CASE("Testing Kruskal's MST algorithm") {
     g.addEdge(2, 3, 5);
 
     list<edge> mst = algorithms::kruskal(g);
-    CHECK(mst.size == 3);
+    CHECK(mst.getSize() == 3);
     int total_weight = 0;
-    for(int i = 0; i < mst.size; i++) {
+    for(int i = 0; i < mst.getSize(); i++) {
         total_weight += mst.get(i).weight;
     }
     CHECK(total_weight == 6);
